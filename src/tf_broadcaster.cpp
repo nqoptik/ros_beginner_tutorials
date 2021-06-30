f8c49c73ff6286c0b329fc85d7865a7910d2ae01
Nguyen Quang <nqoptik@gmail.com>
2021-06-30 16:15:33 +0800

    Quick commit

--- src/tf_broadcaster.cpp
+++ src/tf_broadcaster.cpp
@@ -9,13 +9,19 @@ int main(int argc, char** argv)
     ros::Rate r(100);
 
     tf::TransformBroadcaster broadcaster;
+    tf::Matrix3x3 obs_mat;
+    obs_mat.setEulerYPR(0.0, 3.14, 0.0);
+
+    tf::Quaternion q_tf;
+    obs_mat.getRotation(q_tf);
 
     while (n.ok())
     {
         broadcaster.sendTransform(
             tf::StampedTransform(
-                tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.1, 0.0, 0.2)),
+                tf::Transform(tf::Quaternion(q_tf.getX(), q_tf.getY(), q_tf.getZ(), q_tf.getW()), tf::Vector3(0.1, 0.0, 0.2)),
                 ros::Time::now(), "base_link", "base_laser"));
+        std::cout << q_tf.getX() << " " << q_tf.getY() << " " << q_tf.getZ() << " " << q_tf.getW() << std::endl;
         r.sleep();
     }
 }
